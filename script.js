d8c22cbd010cd80726a5afdd5de57554924e181d
Siddharth Manimaran <79979635+siddharthmanimaran@users.noreply.github.com>
2021-05-11 12:53:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(function() {
+    document.getElementById('hi').innerHTML="10"
+    setTimeout(function() {
+        document.getElementById('hi').innerHTML="9"
+        setTimeout(function() {
+            document.getElementById('hi').innerHTML="8"
+            setTimeout(function() {
+                document.getElementById('hi').innerHTML="7"
+                setTimeout(function() {
+                    document.getElementById('hi').innerHTML="6"
+                    setTimeout(function() {
+                        document.getElementById('hi').innerHTML="5"
+                        setTimeout(function() {
+                            document.getElementById('hi').innerHTML="4"
+                            setTimeout(function() {
+                                document.getElementById('hi').innerHTML="3"
+                                setTimeout(function() {
+                                    document.getElementById('hi').innerHTML="2"
+                                    setTimeout(function() {
+                                        document.getElementById('hi').innerHTML="1"
+                                        setTimeout(function() {
+                                            document.getElementById('hi').innerHTML="happy new year"
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000)
